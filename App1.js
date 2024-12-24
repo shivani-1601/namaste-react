d984cbce74a40bf4327e2c3f6fc2b7e52742243d
sagraw51 <shivani_agrawal@optum.com>
2024-12-24 12:11:37 +0530

    episode 1

---
+++ App1.js
@@ -0,0 +1,14 @@
+const parent = React.createElement(
+    "div",
+    {id : "parent"},
+    React.createElement(
+        "div",
+        {id:"child"},
+        [React.createElement("h1",{},"I am an h1 tag"),
+        React.createElement("h2",{},"I am an h2 tag")]
+
+    )
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
